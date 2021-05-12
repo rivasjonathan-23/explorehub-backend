f257bf7f9192d337d54a233ae5ba607cdf469193
Jonathan Rivas <jonathan@smartstart.us>
2021-05-12 15:45:32 +0800

    revert

--- index.js
+++ index.js
@@ -14,29 +14,6 @@ mongoose.connect(dbConfig.online_db, {
     useFindAndModify: false,
 });
 
-
-const allowedOrigins = [
-    'capacitor://localhost',
-    'ionic://localhost',
-    'http://localhost',
-    'http://localhost:8080',
-    'http://localhost:8100'
-  ];
-  
-  // Reflect the origin if it's in the allowed list or not defined (cURL, Postman, etc.)
-  const corsOptions = {
-    origin: (origin, callback) => {
-      if (allowedOrigins.includes(origin) || !origin) {
-        callback(null, true);
-      } else {
-        callback(new Error('Origin not allowed by CORS'));
-      }
-    }
-  }
-  
-  // Enable preflight requests for all routes
-  app.options('*', cors(corsOptions));
-
 var db = mongoose.connection;
 db.on("connected", () => {
     console.log("connected to database" + dbConfig.online_db);
@@ -60,7 +37,7 @@ app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true, limit: '100mb' }));
 
-app.use("/api",  cors(corsOptions), require("./router/mainRouter"));
+app.use("/api", require("./router/mainRouter"));
 
 let port = process.env.PORT || 3000;
 if (port == null || port == "") {
